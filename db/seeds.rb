87cf22e35549ef3df1df948ea85592afd2acb087
ivoIturrieta <ivoisalinas@gmail.com>
2017-09-25 13:04:00 -0300

    Created seeds file for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,41 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+  Blog.create!(
+    title: "My Blog Post #{blog}",
+    body: "Bacon ipsum dolor amet cow drumstick tenderloin chicken bacon kielbasa
+    andouille short loin frankfurter porchetta burgdoggen. Beef ribs chicken tail
+    chuck kevin. Jerky beef ribs meatloaf ground round, burgdoggen swine ham pork
+    loin doner pork belly brisket picanha leberkas. Turducken cow rump pork loin,
+    jowl pig prosciutto strip steak doner ham hock tongue tenderloin short ribs.
+    Shoulder andouille ham venison t-bone turducken, beef ribs pork belly filet
+    mignon meatloaf leberkas biltong rump ground round cupim. Andouille beef ribs
+    tail, jerky pork chop kevin meatloaf cupim prosciutto shankle chuck frankfurter.
+    Prosciutto flank pork, rump alcatra boudin jerky bresaola cupim corned beef
+    landjaeger kevin tenderloin turducken."
+  )
+end
+puts "10 blog posts created"
+
+5.times do |skill|
+  Skill.create!(
+    title: "Rails #{skill}",
+    percent_utilized: 15
+  )
+end
+
+puts "5 skills created"
+
+9.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title #{portfolio_item}",
+    subtitle: "My greate service",
+    body:"Chuck sausage biltong t-bone pig doner swine porchetta pancetta shank
+     shoulder short loin ribeye ground round. Ground round drumstick capicola
+     ham hock. Tongue corned beef cupim leberkas frankfurter. Ham hock pork loin
+      kevin turkey pork chop meatball short ribs short loin landjaeger andouille
+      sirloin tenderloin salami ribeye swine. Meatloaf picanha pork chop porchetta.
+      Jerky ball tip chuck andouille strip steak chicken jowl.",
+    main_image:"http://via.placeholder.com/600x400",
+    thumb_image: "http://via.placeholder.com/350x200"
+  )
+end
+puts "9 portfolio items created"
