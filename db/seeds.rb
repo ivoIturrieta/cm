c7a30cfba37f2c6edbba327dc5c5e0b6a9ee191a
ivoIturrieta <ivoisalinas@gmail.com>
2017-09-25 23:24:43 -0300

    Implemented custom scopes for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,11 @@
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}"
+  )
+end
+
+puts "3 Topics created"
+
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
@@ -10,7 +18,8 @@
     mignon meatloaf leberkas biltong rump ground round cupim. Andouille beef ribs
     tail, jerky pork chop kevin meatloaf cupim prosciutto shankle chuck frankfurter.
     Prosciutto flank pork, rump alcatra boudin jerky bresaola cupim corned beef
-    landjaeger kevin tenderloin turducken."
+    landjaeger kevin tenderloin turducken.",
+    topic_id: Topic.last.id
   )
 end
 puts "10 blog posts created"
@@ -24,10 +33,10 @@ end
 
 puts "5 skills created"
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
   Portfolio.create!(
     title: "Portfolio title #{portfolio_item}",
-    subtitle: "My greate service",
+    subtitle: "Ruby on Rails",
     body:"Chuck sausage biltong t-bone pig doner swine porchetta pancetta shank
      shoulder short loin ribeye ground round. Ground round drumstick capicola
      ham hock. Tongue corned beef cupim leberkas frankfurter. Ham hock pork loin
@@ -38,4 +47,20 @@ puts "5 skills created"
     thumb_image: "http://via.placeholder.com/350x200"
   )
 end
+
+1.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title #{portfolio_item}",
+    subtitle: "Angular",
+    body:"Chuck sausage biltong t-bone pig doner swine porchetta pancetta shank
+     shoulder short loin ribeye ground round. Ground round drumstick capicola
+     ham hock. Tongue corned beef cupim leberkas frankfurter. Ham hock pork loin
+      kevin turkey pork chop meatball short ribs short loin landjaeger andouille
+      sirloin tenderloin salami ribeye swine. Meatloaf picanha pork chop porchetta.
+      Jerky ball tip chuck andouille strip steak chicken jowl.",
+    main_image:"http://via.placeholder.com/600x400",
+    thumb_image: "http://via.placeholder.com/350x200"
+  )
+end
+
 puts "9 portfolio items created"
