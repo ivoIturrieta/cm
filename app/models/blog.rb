f95360388a02e115e096d1880cb4a6caaddfde58
ivoIturrieta <ivoisalinas@gmail.com>
2017-09-26 13:56:06 -0300

    Refactored strong params

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,9 +1,23 @@
 class Blog < ApplicationRecord
-  enum status: { draft: 0, published: 1}
+  enum status: { draft: 0, published: 1 }
   extend FriendlyId
   friendly_id :title, use: :slugged
 
   validates_presence_of :title, :body
 
   belongs_to :topic
+
+  has_many :comments, dependent: :destroy
+
+  def self.special_blogs
+    all
+  end
+
+  def self.featured_blogs
+    limit(2)
+  end
+
+  def self.recent
+    order("created_at DESC")
+  end
 end
