851d1ef2a5ebcc41ebc9066bca26481a538694b7
ivoIturrieta <ivoisalinas@gmail.com>
2017-09-27 01:24:18 -0300

    Installed petergate gem for auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,18 @@ class ApplicationController < ActionController::Base
   include SetSource
   include CurrentUserConcern
   include DefaultPageContent
+
+  before_action :set_copyright
+
+def set_copyright
+    @copyright = DevcampViewTool::Renderer.copyright 'Ivo Iturrieta', 'All rights reserved'
+  end
+end
+
+module DevcampViewTool
+  class Renderer
+    def self.copyright name, msg
+      "&copy: #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+    end
+  end
 end
