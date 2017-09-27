6a40d90c56c36da2a840e7d59bd5e513f2612141
ivoIturrieta <ivoisalinas@gmail.com>
2017-09-27 00:33:37 -0300

    A bunch of new stuff

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,5 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio_item, only: [:edit, :show, :update, :destroy]
   layout 'portfolio'
   def index
     @portfolio_items = Portfolio.all
@@ -26,12 +27,10 @@ class PortfoliosController < ApplicationController
   end
 
   def edit
-    @portfolio_item = Portfolio.find(params[:id])
+
   end
 
   def update
-    @portfolio_item = Portfolio.find(params[:id])
-
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
@@ -42,11 +41,10 @@ class PortfoliosController < ApplicationController
   end
 
   def show
-    @portfolio_item = Portfolio.find(params[:id])
   end
 
   def destroy
-    @portfolio_item = Portfolio.find(params[:id])
+
     @portfolio_item.destroy
     respond_to do |format|
     format.html { redirect_to portfolios_url, notice: 'Record was successfully removed.' }
@@ -60,5 +58,9 @@ class PortfoliosController < ApplicationController
                                         :body,
                                         technologies_attributes:[:name]
                                         )
+    end
+
+    def set_portfolio_item
+      @portfolio_item = Portfolio.find(params[:id])
     end
 end
