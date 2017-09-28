a49bd54e1b0492b5f025af9c3e27aded7d9f212d
ivoIturrieta <ivoisalinas@gmail.com>
2017-09-28 16:55:37 -0300

    refactor for nav and active status

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -39,11 +39,7 @@ module ApplicationHelper
       {
         url: portfolios_path,
         title: 'Portfolio'
-      },
-      {
-        url: tech_news_path,
-        title: 'Tech News'
-      },
+      }
     ]
   end
 
